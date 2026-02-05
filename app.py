7b7c970405b2c76290547e25b1558a9e0f47a733
Soumyadeep Roy <204379333+SoumyadeepRoy2006@users.noreply.github.com>
2026-02-05 09:21:06 +0530

    Update app.py
    better readability

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from pprint import pprint
 from info_check import get_info
 
-pprint(get_info("https://fastly.picsum.photos/id/900/5000/5000.jpg?hmac=5axe9KJd4BcFevLbLG498ymr5pknvOCw-3Htinb-iGc"))
+info = get_info("https://fastly.picsum.photos/id/900/5000/5000.jpg?hmac=5axe9KJd4BcFevLbLG498ymr5pknvOCw-3Htinb-iGc")
+pprint(info)
