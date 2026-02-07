ed52ed525d14affdb91b9860a032cb2c550f6af2
Soumyadeep Roy <204379333+SoumyadeepRoy2006@users.noreply.github.com>
2026-02-08 00:19:09 +0530

    Create merger.py

---
+++ merger.py
@@ -0,0 +1,14 @@
+def merge_files(files:list, merged_path:str):
+
+   buffer_size = 10 * 1024 * 1024  # 10 MB read and write
+
+   with open(merged_path, "wb") as merged:
+      print("Merging files...")
+      for part in files:
+         with open(part, "rb") as f:
+            while True:
+               chunk = f.read(buffer_size)
+               if not chunk: break
+               merged.write(chunk)
+
+   print("Merging completed!")
