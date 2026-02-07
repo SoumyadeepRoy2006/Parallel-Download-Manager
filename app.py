42b11070a5fc04ee85a2a70ca6c113950218e1bb
Soumyadeep Roy <204379333+SoumyadeepRoy2006@users.noreply.github.com>
2026-02-07 11:11:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from pprint import pprint
 from info_check import get_info
+from partitioner import partition
 
-info = get_info("https://fastly.picsum.photos/id/900/5000/5000.jpg?hmac=5axe9KJd4BcFevLbLG498ymr5pknvOCw-3Htinb-iGc")
-pprint(info)
+PARTS = 10
+
+#info = get_info("https://fastly.picsum.photos/id/900/5000/5000.jpg?hmac=5axe9KJd4BcFevLbLG498ymr5pknvOCw-3Htinb-iGc")
+#pprint(info)
+
+#filesize = 101*1024**2 #1602571
+#
+#partitions = partition(filesize, PARTS)
+#print(partitions)
