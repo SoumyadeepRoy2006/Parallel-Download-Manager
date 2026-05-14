5acb84697445a90593699f8d2a7389d6229714fd
Soumyadeep Roy <204379333+SoumyadeepRoy2006@users.noreply.github.com>
2026-05-14 16:30:49 +0530

    Update File_Merger.py
    attached Bridge

--- Classes/File_Merger.py
+++ Classes/File_Merger.py
@@ -1,14 +1,27 @@
 from os import remove
+
 class MergeJob:
    merged_file_name:str
    merge_dir:str
    part_list:list[str]
-   rw_size = 100 * 1024 * 1024 # 100 MB
+   
+   rw_size = 2 * 1024**2 # MB
+   
+   total_size_bytes:int
+   written_size_bytes = 0
 
-   def __init__(this, merge_dir:str, merged_file_name:str, total_parts:int):
+   bridge = None
+
+   def __init__(this, merge_dir:str, merged_file_name:str, total_parts:int, total_size_bytes:int):
       this.merged_file_name = merged_file_name
       this.merge_dir = merge_dir
       this.part_list = list[str]([None for i in range(total_parts)])
+      this.total_size_bytes = total_size_bytes
+
+
+   def set_bridge(this, bridge):
+      this.bridge = bridge
+
 
    def add_part(this, part_no:int, part_full_path:str):
       if not part_no >= 1:
@@ -16,16 +29,17 @@ class MergeJob:
       else:
          this.part_list[part_no-1] = part_full_path
 
+
    def check_ok(this):
       for i in this.part_list:
          if not i:
             return False
       return True
 
-   def start(this):
-       # 100 MB read and write
 
+   def start(this):
       with open(f"{this.merge_dir}/{this.merged_file_name}", "wb") as merged_file:
+         this.bridge.set_merge_status("start")
          print("Merging files...")
          for part_full_path in this.part_list:
             with open(part_full_path, "rb") as f:
@@ -33,6 +47,10 @@ class MergeJob:
                   buffer = f.read(this.rw_size)
                   if not buffer: break
                   merged_file.write(buffer)
+                  this.written_size_bytes += len(buffer)
+                  this.bridge.set_merge_progress(this.written_size_bytes/this.total_size_bytes)
+         this.bridge.set_merge_status("deleting_parts")
+         for part_full_path in this.part_list:
             remove(part_full_path)
-
+      this.bridge.set_merge_status("end")
       print("Merging completed!")
